9dfea22ed6964ce234e466315cc6549a1cbc2349
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-06-18 14:13:13 +1000

    Add inital models

---
+++ ICOnboardingP1/Models/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICOnboardingP1.Models
+{
+    public class Store
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public ProductSold ProductSold { get; set; }
+    }
+}
