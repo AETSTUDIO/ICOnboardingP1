8896b397a9f61c29636f2880d215f9a237780316
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-06-18 13:07:52 +1000

    Project Initial

---
+++ ICOnboardingP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICOnboardingP1.MvcApplication" Language="C#" %>
---
+++ ICOnboardingP1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICOnboardingP1.Startup))]
+namespace ICOnboardingP1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
